f2209e49ff8a18c40066f5c8aeca4b228a518412
gcw_7zVlCwGb <gcw_7zVlCwGb@noreply.gitcode.com>
2026-09-15 09:30:56 +0800

    注册功能开发结束，实现了用户信息注册到数据库，输入框建议输入用户名密码不能为空，禁止重复注册，注册成功自动跳转，难点是自动跳转，这里原本注册按钮用的是a标签，然后我又往里边写了个doRegister（）的onclick点击事件方法，这个方法里写了setTimeout(function(){window.location.href="login.html";},1000);他和a标签的href冲突了，不得已只能吧a标签的href改成href="#"还得在onclick="doRegister();后面写一个return false;来阻止a标签的href发作，这个大概是注册功能中遇到的比较棘手的问题之一

--- src/main/java/com/qi/empsystem/pojo/User.java
+++ src/main/java/com/qi/empsystem/pojo/User.java
@@ -9,7 +9,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 
 public class User {
+    private Integer id; //int默认为0，Integer默认可以是null，可以区分没赋值和赋值为0
     private String name;
-    private int age;
-    private String sex;
+    private String password;
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
 }
